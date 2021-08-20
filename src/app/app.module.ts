7dc278e41551359863f1ee3897c0358f454f3938
Jaganmohanreddy <jaganmohan.mukkala@gmail.com>
2021-08-20 09:51:25 +0530

    Added 404 error page with CanActivate route guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.component";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { CreateEventComponent } from "./events/create-event.component";
+import { Error404Component } from "./errors/404.component";
 import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./commom/toastr.service";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./routes";
+import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 @NgModule({
   declarations: [
     EventsAppComponent,
@@ -18,10 +20,11 @@ import { appRoutes } from "./routes";
     EventThumbnailComponent,
     EventDetailsComponent,
     CreateEventComponent,
+    Error404Component,
     NavBarComponent
   ],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [EventService, ToastrService],
+  providers: [EventService, ToastrService, EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
---
+++ src/app/events/event-details/event-route-activator.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, ActivatedRouteSnapshot, Router } from "@angular/router";
+import { EventService } from "../shared/event.service";
+
+@Injectable()
+export class EventRouteActivator implements CanActivate {
+  constructor(private eventService: EventService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot) {
+    const eventExists = !!this.eventService.getEvent(+route.params["id"]);
+    if (!eventExists) {
+      this.router.navigate(["404"]);
+    }
+    return eventExists;
+  }
+}
