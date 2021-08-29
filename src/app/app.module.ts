6143e59644bcd2a0386bd242c66d6beafea74696
Jaganmohanreddy <jaganmohan.mukkala@gmail.com>
2021-08-29 20:28:36 +0530

    Added shared exports for better management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from "./events/index";
 import { EventsAppComponent } from "./events-app.component";
-import { EventsListComponent } from "./events/events-list.component";
-import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.component";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from "./errors/404.component";
-import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./commom/toastr.service";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./routes";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
-import { EventListResolver } from "./events/events-list-resolver.service";
+
 @NgModule({
   declarations: [
     EventsAppComponent,
